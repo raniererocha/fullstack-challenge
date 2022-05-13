9c483a46a6fd0b9c88048208f1cb6fc9fbd7079a
Raniere Rocha <rlsmont123@hotmail.com>
2022-05-13 09:11:41 -0300

    refact: signup page

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,20 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useNavigate} from 'react-router-dom'
 import * as C from './style'
 
 export default function SignUp() {
 
+    const [inputName, setInputName] = useState<string>('')
+    const [inputEmail, setInputEmail] = useState<string>('')
+    const [inputPassword, setInputPassword] = useState<string>('')
+    const [inputCoPassword, setInputCoPassword] = useState<string>('')
+
     const navigate = useNavigate()
+
+    const handleSingUp = async (evt) => {
+        evt.preventDefault()
+    }
 
     return(
         <C.SignUpContainer>
             <C.SignUpForm>
                 <h1>Cadastro</h1>
-                <C.SignUpInput type="text" placeholder="Nome" />
-                <C.SignUpInput type="email" placeholder="Email" />
-                <C.SignUpInput type="password" placeholder="Senha" />
-                <C.SignUpInput type="password" placeholder="Confirmação da Senha" />
-                <C.SignUpBtn>Cadastrar</C.SignUpBtn>
+                <C.SignUpInput 
+                    type="text" 
+                    placeholder="Nome" 
+                    value={inputName}
+                    onChange={ evt => setInputName(evt.target.value) }
+                />
+                <C.SignUpInput 
+                    type="email" 
+                    placeholder="Email" 
+                    value={inputEmail}
+                    onChange={ evt => setInputEmail(evt.target.value) }
+                />
+                <C.SignUpInput 
+                    type="password" 
+                    placeholder="Senha" 
+                    value={inputPassword}
+                    onChange={ evt => setInputPassword(evt.target.value) }
+                />
+                <C.SignUpInput 
+                    type="password" 
+                    placeholder="Confirmação da Senha" 
+                    value={inputCoPassword}
+                    onChange={ evt => setInputCoPassword(evt.target.value) }
+                />
+                <C.SignUpBtn onClick={ handleSignUp} >Cadastrar</C.SignUpBtn>
                 <C.SignUpBtnLink onClick={ evt => {
                     evt.preventDefault()
                     navigate('/signin')
